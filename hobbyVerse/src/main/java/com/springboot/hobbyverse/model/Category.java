8c515a6e8c37a0dcc2e406e2a25a14fbc4529a0b
alwdb <alwdb7226@gamil.com>
2025-02-08 09:42:23 +0900

    2/8 조미주 업로드

---
+++ hobbyVerse/src/main/java/com/springboot/hobbyverse/model/Category.java
@@ -0,0 +1,11 @@
+package com.springboot.hobbyverse.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+	private Integer category ;
+	private String nameString;
+}
